7967c0338147890fb6b1d0b43732e037ce132688
Ali Modaresi <amodaresi@corporatings.com>
2024-11-22 14:51:00 +0100

    convert to normal dict when exception comes

--- mpipeline/worker_exception.py
+++ mpipeline/worker_exception.py
@@ -11,18 +11,28 @@ tblib.pickling_support.install()
 T = TypeVar('T')
 
 
+def to_dict_recursive(obj):
+    if isinstance(obj, dict):
+        return {k: to_dict_recursive(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [to_dict_recursive(v) for v in obj]
+    else:
+        return obj
+
+
 class WorkerException(Exception, Generic[T]):
     stage: Any
     work_item: T | None
     tb_frame: Any
 
     def __init__(self, orig_exc: BaseException, stage: Any, work_item: T | None, shared_data: DictProxy | None):
+        if shared_data is not None:
+            self.shared_data = to_dict_recursive(shared_data)
         super().__init__(orig_exc, str(stage), work_item, shared_data)
         self.tb_frame = orig_exc.__traceback__
         self.orig_exc = orig_exc
         self.work_item = work_item
         self.stage = str(stage)
-        self.shared_data = shared_data
 
     def __str__(self):
         return f"WorkerException {self.orig_exc}\n\t\tstage: {self.stage}\n\t\twork_item: {self.work_item}\n\t\tshared_data: {self.shared_data}"
