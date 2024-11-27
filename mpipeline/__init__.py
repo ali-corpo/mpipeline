5792edda87140a861102486c573dd4dc3d965a63
Ali Modaresi <amodaresi@corporatings.com>
2024-11-27 11:06:28 +0100

    add ThreadSafeDict

--- mpipeline/__init__.py
+++ mpipeline/__init__.py
@@ -1,4 +1,5 @@
-from .pipeline import Pipeline, Stage
+from .pipeline import Pipeline
+from .pipeline import Stage
+from .thread_safe_dict import ThreadSafeDict
 from .worker import Worker
-
-__all__ = ['Pipeline', 'Worker', 'Stage']
+__all__ = ['Pipeline', 'Worker', 'Stage', 'ThreadSafeDict']
