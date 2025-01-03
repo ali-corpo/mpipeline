0f3c60f3662bf830531013cdd78d925d6d950336
Ali Modaresi <amodaresi@corporatings.com>
2025-01-03 11:01:52 +0100

    fix: Worker Exception Traceback

--- mpipeline/worker_exception.py
+++ mpipeline/worker_exception.py
@@ -5,10 +5,8 @@ from typing import Any
 from typing import Generic
 from typing import TypeVar
 
-import tblib.pickling_support
+from tblib import Traceback
 
-
-tblib.pickling_support.install()
 
 T = TypeVar('T')
 
@@ -25,20 +23,27 @@ def to_dict_recursive(obj):
 class WorkerException(Exception, Generic[T]):
     stage: Any
     work_item: T | None
-    tb_frame: Any
+    tb_frame: dict | None
 
-    def __init__(self, orig_exc: BaseException, stage: Any, work_item: T | None, shared_data: dict | None):
-
-        self.shared_data = to_dict_recursive(shared_data)
-        super().__init__(orig_exc, str(stage), work_item, self.shared_data)
-        self.tb_frame = orig_exc.__traceback__
-        self.orig_exc = orig_exc
+    def __init__(self, orig_exc: BaseException, stage: Any, work_item: T | None, shared_data: dict | None, tb_frame: dict | None = None):
+        shared_data2 = to_dict_recursive(shared_data)
+        try:
+            pickle.dumps(shared_data2)
+            self.shared_data = shared_data2
+        except BaseException:
+            self.shared_data = None
         try:
             pickle.dumps(work_item)
             self.work_item = work_item
-        except Exception:
+        except BaseException:
             self.work_item = None
         self.stage = str(stage)
+
+        self.tb_frame = tb_frame or Traceback(orig_exc.__traceback__).as_dict()
+
+        self.orig_exc = orig_exc
+
+        super().__init__(self.orig_exc, self.stage, self.work_item, self.shared_data, self.tb_frame)
 
     def __str__(self):
         return f"{type(self.orig_exc)} {self.orig_exc}\n\t\tstage: {self.stage}\n\t\twork_item: {self.work_item}\n\t\tshared_data: {self.shared_data}"
@@ -46,4 +51,5 @@ class WorkerException(Exception, Generic[T]):
     def re_raise(self):
         if self.tb_frame is None:
             raise self
-        raise self.with_traceback(self.tb_frame)
+        # traceback.print_tb(Traceback.from_dict(self.tb_frame).as_traceback())
+        raise self.with_traceback(Traceback.from_dict(self.tb_frame).as_traceback())
